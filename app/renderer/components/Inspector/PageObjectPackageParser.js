e853d2c0984e352a5543826d3cd433a7b3489e7b
helenren <qren@salesforce.com>
2022-09-08 15:57:08 -0700

    add root selector info for root po

--- app/renderer/components/Inspector/PageObjectPackageParser.js
+++ app/renderer/components/Inspector/PageObjectPackageParser.js
@@ -36,62 +36,92 @@ export default class PageObjectPackageParser {
     }
   }
 
+  parseInterfaces (pageObjectName, rootNode) {
+    const methods = [];
+    const children = [];
+    const rootSelector = {ios: '', android: ''};
+
+    const methodsNode = findNodeAtLocation(rootNode, ['methods']);
+    if (methodsNode && methodsNode.children) {
+      methodsNode.children.forEach((methodObjectNode) => {
+        // Get all methods info
+        const name = findNodeAtLocation(methodObjectNode, ['name']);
+        if (name && name.value.length !== 0) {
+          methods.push(name.value);
+        }
+
+        // Get all children info
+        const returnTypeNode = findNodeAtLocation(methodObjectNode, ['returnType']);
+        if (returnTypeNode) {
+          const returnTypeNodeValue = getNodeValue(returnTypeNode);
+          if (isCustomType(returnTypeNodeValue)) {
+            const typeValue = returnTypeNodeValue.split('/').pop();
+            children.push(new PageObjectTreeNode(typeValue, rootSelector, methods, children));
+          }
+        }
+      });
+    }
+
+    // For root Page Object
+    const rootMarkerNode = findNodeAtLocation(rootNode, ['root']);
+    if (rootMarkerNode !== undefined && rootMarkerNode.value === true) {
+      const rootNode = new PageObjectTreeNode(pageObjectName, rootSelector, methods, children);
+      if (!this.rootMap.has(pageObjectName)) {
+        this.rootMap.set(pageObjectName, rootNode);
+      }
+    } else {
+      // For non-root Page Object
+      const childNode = new PageObjectTreeNode(pageObjectName, rootSelector, methods, children);
+      for (let [key, value] of this.rootMap) {
+        if (value.length !== 0) {
+          const existingChildren = value.children;
+          for (let i = 0; i < existingChildren.length; i++) {
+            if (existingChildren[i].name === pageObjectName) {
+              existingChildren[i] = childNode;
+              value.children = existingChildren;
+              this.rootMap.set(key, value);
+              // Get out from the inside for loop
+              i = existingChildren.length;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  parseImpls (pageObjectName, rootNode) {
+    const rootMarkerNode = findNodeAtLocation(rootNode, ['root']);
+    if (rootMarkerNode !== undefined && rootMarkerNode.value === true) {
+      const rootSelectorNode = findNodeAtLocation(rootNode, ['selector']);
+      const profileNode = findNodeAtLocation(rootNode, ['profile']);
+      if (rootSelectorNode && profileNode.children && this.rootMap.has(pageObjectName)) {
+        const { methods, children, rootSelector } = this.rootMap.get(pageObjectName);
+        const platformNode = profileNode.children[0];
+        const platform = platformNode.children[0].children[1].children[0].value.split('_')[0].toLowerCase();
+        const selector = rootSelectorNode.children[0].children[1].value;
+        if (platform === 'ios') {
+          rootSelector.ios = selector;
+        } else if (platform === 'android') {
+          rootSelector.android = selector;
+        }
+        const node = new PageObjectTreeNode(pageObjectName, rootSelector, methods, children);
+        this.rootMap.set(pageObjectName, node);
+      }
+    }
+  }
+
   findRoot (filePath) {
     const [pageObjectName] = basename(filePath).split('.');
     const sourceText = readFileSync(filePath, 'utf8');
     const rootNode = parseTree(sourceText, []);
     const interfaceNode = findNodeAtLocation(rootNode, ['interface']);
-    // Only parse the interface file
+    const implNode = findNodeAtLocation(rootNode, ['implements']);
+    // Parse the interface files
     if (interfaceNode !== undefined && interfaceNode.value === true) {
-      const methodsNode = findNodeAtLocation(rootNode, ['methods']);
-
-      const methods = [];
-      const children = [];
-      if (methodsNode && methodsNode.children) {
-        methodsNode.children.forEach((methodObjectNode) => {
-          // Get all methods name
-          const name = findNodeAtLocation(methodObjectNode, ['name']);
-          if (name && name.value.length !== 0) {
-            methods.push(name.value);
-          }
-
-          // Get all children info
-          const returnTypeNode = findNodeAtLocation(methodObjectNode, ['returnType']);
-          if (returnTypeNode) {
-            const returnTypeNodeValue = getNodeValue(returnTypeNode);
-            if (isCustomType(returnTypeNodeValue)) {
-              const typeValue = returnTypeNodeValue.split('/').pop();
-              children.push(new PageObjectTreeNode(typeValue, []));
-            }
-          }
-        });
-      }
-
-      // For root Page Object
-      const rootMarkerNode = findNodeAtLocation(rootNode, ['root']);
-      if (rootMarkerNode !== undefined && rootMarkerNode.value === true) {
-        const rootNode = new PageObjectTreeNode(pageObjectName, methods, children);
-        if (!this.rootMap.has(pageObjectName)) {
-          this.rootMap.set(pageObjectName, rootNode);
-        }
-      } else {
-        // For non-root Page Object
-        const childNode = new PageObjectTreeNode(pageObjectName, methods, children);
-        for (let [key, value] of this.rootMap) {
-          if (value.length !== 0) {
-            const existingChildren = value.children;
-            for (let i = 0; i < existingChildren.length; i++) {
-              if (existingChildren[i].name === pageObjectName) {
-                existingChildren[i] = childNode;
-                value.children = existingChildren;
-                this.rootMap.set(key, value);
-                // Get out from the inside for loop
-                i = existingChildren.length;
-              }
-            }
-          }
-        }
-      }
+      this.parseInterfaces(pageObjectName, rootNode);
+    } else if (implNode !== undefined && implNode.value.length !== 0) {
+      // Parse the implementation files
+      this.parseImpls(implNode.value.split('/').pop(), rootNode);
     }
   }
 
